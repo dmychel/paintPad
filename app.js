7e347c6c0695bf42ed5b6202baec2197748d3682
dmychel <dmychel@tutanota.com>
2023-03-17 10:06:38 -0400

    foundation
    defined variables and adding functions I know I will need.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+const blackBrush = document.getElementById('black');
+const tealBrush = document.getElementById('teal');
+const greenBrush = document.getElementById('green');
+const pinkBrush = document.getElementById('pink');
+const rainbowBrush = document.getElementById('rainbow');
+const eraser = document.getElementById('eraser');
+
+
+// make grid function
+function makeGrid(){}
+// paint function
+blackBrush.addEventListener('click', () =>{});
+
+tealBrush.addEventListener('click', () =>{});
+
+greenBrush.addEventListener('click', () =>{});
+
+pinkBrush.addEventListener('click', () => {});
+
+rainbowBrush.addEventListener('click', () =>{});
+
+eraser.addEventListener('click', () =>{})
+
+
+// clear function
+
+
